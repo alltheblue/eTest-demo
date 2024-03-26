602fdd71fd630b6844ae779a7b204d7594d5c62f
haibo.li2 <haibo.li2@nio.com>
2024-03-26 13:03:30 +0800

    init demo

---
+++ 2e6fdbc5-76c3-4f15-b78c-172e3aa29d4d/ete/globalConfig.js
@@ -0,0 +1,1 @@
+{"config":[{"id":"$VAR88affc11-8a99-4ad0-90a6-5a41027df2a4","souce":"config","date":1710992640498,"name":"222","mode":"code2","merge":"suffix","describe":"222"}]}
---
+++ 2e6fdbc5-76c3-4f15-b78c-172e3aa29d4d/ete/mockConfig.js
@@ -0,0 +1,9 @@
+[
+  {
+    "url": "xxx.xxx.come/aa/bb/",
+    "status": 200,
+    "method": "GET",
+    "contentType": "application/json",
+    "body": {}
+  }
+] 
